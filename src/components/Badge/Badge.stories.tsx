e7d674d593b99e70a71c3c249fbb5806f799c276
Mohamed Ahmed <mohamed.ahmed@plugsurfing.com>
2023-02-08 14:26:15 +0100

    Build stories for Badge and Review components

---
+++ src/components/Badge/Badge.stories.tsx
@@ -0,0 +1,17 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+import { Badge } from './Badge'
+
+// Metadata of our component
+export default {
+  title: 'Components/Badge',
+  component: Badge,
+  args: {
+    text: 'Comfort food',
+  },
+} as ComponentMeta<typeof Badge>
+
+// Base Template
+const Template: ComponentStory<typeof Badge> = (args) => <Badge {...args} />
+
+export const Default = Template.bind({})
