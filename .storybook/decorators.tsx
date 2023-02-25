4d29a88b80b4984c86a6abe9c7fecd1f168d2794
Mohamed Ali <a.mohamed.cali@gmail.com>
2023-02-25 16:52:54 +0100

    Add Home page to Storybook

--- .storybook/decorators.tsx
+++ .storybook/decorators.tsx
@@ -1,9 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit'
 import { DecoratorFn } from '@storybook/react'
 import { initialize, mswDecorator } from 'msw-storybook-addon'
 import React from 'react'
+import { Provider as StoreProvider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { withDesign } from 'storybook-addon-designs'
 import { ThemeProvider } from 'styled-components'
+
+import { rootReducer } from '../src/app-state'
 import { GlobalStyle } from '../src/styles/GlobalStyle'
 import { darkTheme, lightTheme } from '../src/styles/theme'
 
@@ -29,4 +33,16 @@ const withTheme: DecoratorFn = (StoryFn, context) => {
   )
 }
 
-export const globalDecorators = [withTheme, withDesign, withRouter, mswDecorator]
+const withStore: DecoratorFn = (StoryFn, { parameters }) => {
+  const store = configureStore({
+    reducer: rootReducer,
+    preloadedState: parameters.store?.initialState,
+  })
+  return (
+    <StoreProvider store={store}>
+      <StoryFn />
+    </StoreProvider>
+  )
+}
+
+export const globalDecorators = [withTheme, withDesign, withRouter, mswDecorator, withStore]
